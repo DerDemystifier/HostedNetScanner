e5b9474d5f20c1c398eac67f6b94e20e433f7a01
Bassim EL BAKKALI EL GAZUANI <124774256+DerDemystifier@users.noreply.github.com>
2024-08-17 17:53:47 +0100

    Add status icons to device table in MainWindow; update status display with appropriate icons for device status

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Set;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -20,6 +22,11 @@ public class MainWindow extends JFrame {
 	private JTable table;
 	private JMenuItem mntmStartNetwork;
 
+	// Add status icons
+	private final ImageIcon statusGreen = new ImageIcon("images/green16.png");
+	private final ImageIcon statusYellow = new ImageIcon("images/yellow16.png");
+	private final ImageIcon statusRed = new ImageIcon("images/red16.png");
+
 	/**
 	 * Listener that updates the table with the active devices on the network. This
 	 * listener is triggered when the network is updated.
@@ -30,7 +37,23 @@ public class MainWindow extends JFrame {
 			DefaultTableModel model = (DefaultTableModel) table.getModel();
 			model.setRowCount(0);
 			for (Device device : knownDevices) {
-				model.addRow(new Object[] { device.getStatus(), device.getHostname(), device.getCustomName(),
+				// Convert status to appropriate icon
+				ImageIcon statusIcon;
+				switch (device.getStatus().toLowerCase()) {
+				case "connected":
+					statusIcon = statusGreen;
+					break;
+				case "unconfirmed":
+					statusIcon = statusYellow;
+					break;
+				case "offline":
+					statusIcon = statusRed;
+					break;
+				default:
+					statusIcon = statusRed;
+				}
+
+				model.addRow(new Object[] { statusIcon, device.getHostname(), device.getCustomName(),
 						device.getMacAddress(), device.getHostAddress(), device.getFormattedConnectionTime(),
 						device.getFormattedLastSeen() });
 			}
@@ -107,24 +130,21 @@ public class MainWindow extends JFrame {
 		table.setModel(new DefaultTableModel(new Object[][] { { null, null, null, null, null, null, null }, },
 				new String[] { "Status", "Hostname", "Custom name", "MAC Address", "IP Address", "Connection time",
 						"Last Seen" }) {
-			Class[] columnTypes = new Class[] { Object.class, String.class, String.class, String.class, String.class,
+			Class[] columnTypes = new Class[] { Icon.class, String.class, String.class, String.class, String.class,
 					Object.class, String.class };
 
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
-
-			boolean[] columnEditables = new boolean[] { false, false, false, false, false, false, false };
-
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
 		});
 		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(0).setMaxWidth(46);
 		table.getColumnModel().getColumn(1).setResizable(false);
 		table.getColumnModel().getColumn(2).setResizable(false);
 		table.getColumnModel().getColumn(3).setResizable(false);
 		table.getColumnModel().getColumn(4).setResizable(false);
+		table.getColumnModel().getColumn(4).setMinWidth(120);
+		table.getColumnModel().getColumn(4).setMaxWidth(120);
 		table.getColumnModel().getColumn(5).setResizable(false);
 		table.getColumnModel().getColumn(6).setResizable(false);
 		scrollPane.setViewportView(table);
